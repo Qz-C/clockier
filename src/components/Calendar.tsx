a04fda279cc52475bcfd1ff6be94901c46465e1d
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2021-02-19 19:45:34 -0300

    add toggle functionality

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
     StyleSheet,
     Text,
@@ -36,33 +36,47 @@ const weekDays: WeekDays[] = [
     { day: "Friday", short: "Fri" },
     { day: "Saturday", short: "Sat" },
 ]
+
+interface date {
+    day: number,
+    toggleable: boolean,
+    toggled: boolean,
+    date: Date,
+    defaultStyles: any,
+    textStyle: any,
+    toggleStyle?: any,   
+}
+
+
 interface Props {
 
 }
 
 const Calendar: React.FC<Props> = (props) => {
 
-    const [month, setMonth] = useState(new Date().getMonth());
-    const [year, setYear] = useState(new Date().getFullYear());
+    const [month, setMonth] = useState<number>(new Date().getMonth());
+    const [year, setYear] = useState<number>(new Date().getFullYear());
+    const [toogled, setToogled] = useState<boolean>(false);
 
-    const onToogle = ():any => ({
-        backgroundColor: "red",
-        borderRadius: 50
-    })
-
-    const genarteMonth = (firstDay: Date): date[] => {
-        const month: number = firstDay.getMonth();
+    const generateMonth = (): date[] => {
+        const firstDay = new Date(year, month, 1, 0, 0, 0)
         const pastMonthLastDay: Date = new Date(firstDay.getTime() - oneDayInMs);
 
         const pastMonthDays: date[] = [];
         for (let i = (pastMonthLastDay.getDate() - firstDay.getDay()) + 1; i <= pastMonthLastDay.getDate(); i++)
             pastMonthDays.push({
                 day: i,
-                toggle: false,
-                date: new Date(year, month, i, 0,0,0),
-                styles:{
-                    color:"#C0C0C0",
-                    fontFamily: "Nunito-Bold",
+                toggleable: false,
+                toggled: false,
+                date: new Date(year, month, i, 0, 0, 0),
+                defaultStyles: {
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: '#ffffff',
+                },
+                textStyle:{
+                    color: "#808080",
+                    fontFamily: "Nunito-Regular",
                     fontSize: 16,
                 }
             })
@@ -72,12 +86,24 @@ const Calendar: React.FC<Props> = (props) => {
         while (newDay.getMonth() === month) {
             currentMonthDays.push({
                 day: newDay.getDate(),
-                toggle: false,
+                toggled: false,
+                toggleable: true,
                 date: newDay,
-                styles:{
+                defaultStyles: {
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: '#ffffff',
+                },
+                textStyle:{
                     color: "#000000",
                     fontFamily: "Nunito-Bold",
                     fontSize: 16,
+                },
+                toggleStyle:{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    borderRadius: 20,
+                    backgroundColor: "red"
                 }
             });
             newDay = new Date(newDay.getTime() + oneDayInMs);
@@ -86,18 +112,32 @@ const Calendar: React.FC<Props> = (props) => {
         const nextMonthDays: date[] = [];
         for (let i = 0; i < daysPerPage - (currentMonthDays.length + pastMonthDays.length); i++)
             nextMonthDays.push({
-                day: i+1,
-                toggle: false,
-                date: new Date(year, month, i + 1, 0,0,0),
-                styles: {
-                    color:"#C0C0C0",
-                    fontFamily: "Nunito-Bold",
+                day: i + 1,
+                toggled: false,
+                toggleable: false,
+                date: new Date(year, month, i + 1, 0, 0, 0),
+                defaultStyles: {
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: '#ffffff',
+                },
+                textStyle:{
+                    color: "#808080",
+                    fontFamily: "Nunito-Regular",
                     fontSize: 16,
                 }
             });
-
-        return pastMonthDays.concat(currentMonthDays, nextMonthDays);
+        return (pastMonthDays.concat(currentMonthDays, nextMonthDays));
     }
+
+    const [dates, setDates] = useState<date[]>(generateMonth());
+
+    useEffect(() => {
+        setDates(generateMonth());
+        console.log("UseEffect");
+    }, [year, month]);
+
+
 
     const nextMonth = (): void => {
         if (month === 11) {
@@ -115,63 +155,92 @@ const Calendar: React.FC<Props> = (props) => {
             setMonth(month - 1);
     }
 
-    const calendarLayout = (dates: date[]): any => {
+    const onToggle = (toggleIndex: any) => {
+
+        //Spread syntax creates a new array, 
+        //it does not only passes by reference, 
+        //without it the state wouldn't update, 
+        //once the memory address is would be the very same.
+        let temp: date[] = [...dates];
+    
+        temp.map((date:date, index) => {
+            if(index === toggleIndex){
+                if(date.toggleable)
+                    date.toggled ? date.toggled = false : date.toggled = true;
+            }else{
+                if(date.toggled)
+                    date.toggled = false
+            }
+
+            return date;
+        })
+
+        setDates(temp);
+    }
+
+    const calendarLayout = (): any => {
+        generateMonth();
+        //const dates = generateMonth();
         const rows = 6;
         const columns = 7;
         const layout: any = [];
         for (let i = 1; i < rows; i++) {
             const currentRow = [];
             for (let j = (i * columns) - columns; j < i * columns; j++) {
-                const component = (<Col style={styles.cell}>
-                                        <TouchableHighlight>
-                                            <Text style={dates[j].styles}> {dates[j].day} </Text>
-                                        </TouchableHighlight>
-                                </Col>);
+
+                const background = dates[j].toggled ? dates[j].toggleStyle : dates[j].defaultStyles; 
+                const component = (<Col key={j} style={background}>
+                    <TouchableHighlight onPress={()=>onToggle(j)}>
+                            <Text style={dates[j].textStyle}> {dates[j].day} </Text>
+                    </TouchableHighlight>
+                </Col>);
                 currentRow.push(component);
             }
             layout.push(currentRow);
         }
+
         return (layout);
     }
 
-        const dates: date[] = genarteMonth(new Date(year, month, 1, 0, 0, 0));
+    
+    //const dates: date[] = generateMonth(new Date(year, month, 1, 0, 0, 0));
 
-        return (
-            <View style={styles.calendar}>
-                <View style={styles.header}>
-                    <TouchableHighlight onPress={previousMonth}>
-                        <Icon name="navigate-before" size={30} color="#000000" />
-                    </TouchableHighlight>
-                    <View style={styles.title}>
-                        <Text style={styles.titleText}> {months[month].month} </Text>
-                        <Text style={styles.titleText}> {year} </Text>
-                    </View>
-                    <TouchableHighlight onPress={nextMonth}>
-                        <Icon name="navigate-next" size={30} color="#000000" />
-                    </TouchableHighlight>
+    return (
+        <View style={styles.calendar}>
+            <View style={styles.header}>
+                <TouchableHighlight onPress={previousMonth}>
+                    <Icon name="navigate-before" size={30} color="#000000" />
+                </TouchableHighlight>
+                <View style={styles.title}>
+                    <Text style={styles.titleText}> {months[month].month} </Text>
+                    <Text style={styles.titleText}> {year} </Text>
                 </View>
-                <Grid>
-                    <Row>
-                        {weekDays.map(day => (
-                            <Col style={styles.cell}>
-                                <Text style={styles.weekDaysText}>{day.short}</Text>
-                            </Col>
-                        ))}
-                    </Row>
-                    {calendarLayout(dates).map((day: date, index: any) => (
-                        <Row key={index}>
-                            {day}
-                        </Row>
-                    ))}
-                </Grid>
+                <TouchableHighlight onPress={nextMonth}>
+                    <Icon name="navigate-next" size={30} color="#000000" />
+                </TouchableHighlight>
             </View>
-        )
+            <Grid>
+                <Row>
+                    {weekDays.map((day, index) => (
+                        <Col key={index} style={styles.cell}>
+                            <Text style={styles.weekDaysText}>{day.short}</Text>
+                        </Col>
+                    ))}
+                </Row>
+                {calendarLayout().map((day: date, index: any) => (
+                    <Row key={index}>
+                        {day}
+                    </Row>
+                ))}
+            </Grid>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     calendar: {
-        width: "100%",
-        height: "100%",
+        width: "90%",
+        height: "50%",
         backgroundColor: "#ffffff"
     },
     header: {
@@ -192,13 +261,10 @@ const styles = StyleSheet.create({
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
-        borderColor: "black",
-        borderWidth: 1
     },
     weekDaysText: {
         fontFamily: "Nunito-Black",
         fontSize: 16,
-        backgroundColor: "red"
     },
 
 })
